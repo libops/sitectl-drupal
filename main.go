92e66e37d14eade2b47422f870f6c991c61007a2
Joe Corall <joe@libops.io>
2026-03-21 08:05:41 -0400

    Publish linux packages

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/libops/sitectl-drupal/cmd"
 	"github.com/libops/sitectl/pkg/plugin"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
 )
 
 func main() {
 	sdk := plugin.NewSDK(plugin.Metadata{
 		Name:        "drupal",
-		Version:     "v0.0.3",
+		Version:     fmt.Sprintf("%s (Built on %s from Git SHA %s)", version, date, commit),
 		Description: "Drupal utilities and migration tools",
 		Author:      "libops",
 	})
